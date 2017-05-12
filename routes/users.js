a7acdde9e8a85511e077c0060cd018d7cbe94af5
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-05-11 19:43:01 -0500

    cleaning user methods

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,8 @@
 /**
  * Module dependencies.
  */
-var logger = require('../config/logger').logger;
+var logger = require('../utils/logger').logger;
 var User = require('../models/users').User;
-var UserReset = require('../models/userReset').UserReset;
 
 // ENDPOINT: /users METHOD: GET
 exports.getUsers = function(req, res){
@@ -71,102 +70,6 @@ exports.getLogin = function (req, res) {
         }
         // success
         res.json({ message:"Login authenticated successfully", data: user });
-    });
-};
-
-// ENDPOINT: /password/reset METHOD: POST
-exports.postPasswordReset = function (req, res) {
-    var emailRequest = req.body.email;
-    // Use the 'User' model to find one user with this email address
-    // Validate email provided exist on database
-    User.find({ email: emailRequest }, function (err, user) {
-        // Check for errors and show message
-        if(err){
-            logger.error(err);
-            res.json({ message: 'Email provided doesn`t Exists'});
-            //res.send(err);
-            return;
-        }
-
-        // Get User data values
-        var idUser = user[0]._id;
-        var fullName = user[0].name + ' ' + user[0].lastName;
-
-        // Generate random code, with the size of 8 characters mixing numbers and letters
-        var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        var sizeCode = 8;
-        var codeResult = '';
-        for (var i = sizeCode; i > 0; --i) codeResult += chars[Math.round(Math.random() * (chars.length - 1))];
-        //logger.info(codeResult);
-
-        //Get current year
-        var currentYear = new Date().getFullYear();
-        //logger.info(currentYear);
-
-        // Get sent_at date
-        //var sendAtDate = '2015-01-01 12:12:12';
-        //logger.info(sendAtDate);
-
-        res.json({ message: 'Email sent successfully!, ' + codeResult });
-    });
-};
-
-// ENDPOINT: /password/reset/:code METHOD: PATCH
-exports.PatchPasswordReset = function (req, res) {
-    var codeRequest = req.params.code;
-    // Use the 'UserReset' model to find one user with this email address
-    UserReset.find({code: codeRequest}, function (err, userReset) {
-        // Check for errors and show message
-        if(err){
-            logger.error(err);
-            res.json({ message: 'Code doesn`t exists'});
-            res.send(err);
-            return;
-        }
-
-        // Change the password for the new sent
-        //var emailRequest = req.body.email;
-        //logger.info(emailRequest);
-        var passRequest = req.body.password;
-        logger.info(passRequest);
-        //logger.info(userReset[0].idUser);
-        var idUserRequest = userReset[0]._id;
-
-        // Find the user and update de password value
-        User.findById(userReset[0].idUser, function (err, user) {
-            if(err){
-                logger.error(err);
-                res.send(err);
-                return;
-            }
-
-            // Change the password value
-            user.password = passRequest;
-
-            user.save(function(err){
-                // Check for errors and show message
-                if(err){
-                    logger.error(err);
-                    res.send(err);
-                    return;
-                }
-                // Now deleted the code from the collection
-                UserReset.findByIdAndRemove(idUserRequest, function(err){
-                    // Check for errors and show message
-                    if(err){
-                        logger.error(err);
-                        res.send(err);
-                        return;
-                    }
-                    // success
-                    // success
-                    res.json({ message: 'User password changed and code: ' + codeRequest +' deleted successfully!' });
-                });
-            });
-
-
-
-        });
     });
 };
 
