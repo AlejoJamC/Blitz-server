4aef2fb19fcc439516feb1babe66a93e7c8132cd
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-09-14 23:55:33 -0500

    new murmur feather service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const users = require('./users/users.service.js');
+const murmurs = require('./murmurs/murmurs.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
+  app.configure(murmurs);
 };
